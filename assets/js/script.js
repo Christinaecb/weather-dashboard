62e32f014363b14caac5ea7be71565766cc3e1b5
Your Name <you@example.com>
2023-01-28 18:01:56 -0500

    added form submission in js to prevent reloading the page and grab the value contained in the search field

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,10 @@ var msg = document.querySelector(".top-banner .msg");
 var list = document.querySelector(".ajax-section .cities");
 
 var apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+//Stops the form from submitting and grabs the value contained in the search field
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let inputVal = input.value;
+  
+   
