0193bc51b8ed3aa81cc2daba9518f446e57ecc97
Your Name <you@example.com>
2023-01-28 17:55:24 -0500

    created vars in js file and linked js to index.html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*Search by using city name (e.g. london) or a comma-separated city name along with the country code (e.g. london,ca)*/
+var form = document.querySelector(".top-banner form");
+var input = document.querySelector(".top-banner input");
+var msg = document.querySelector(".top-banner .msg");
+var list = document.querySelector(".ajax-section .cities");
+
+var apiKey = "4d8fb5b93d4af21d66a2948710284366";
